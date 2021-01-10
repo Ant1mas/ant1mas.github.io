f37377ac96b4d5bee5222f8baac8181af39992ad
ANT1MAS-PC\anton <anton.web@mail.ru>
2021-01-10 23:21:11 +0500

    (number-to-words-ru) try to deploy

---
+++ number-to-words-ru/_next/static/CCnBQraoKvv0uZ1xAWbs4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F29107295.52ea5967da8fcf201115.js","static\u002Fchunks\u002Fpages\u002Findex-96c25873eaa95f15f943.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-154b2ec68bf9e7204aeb.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
