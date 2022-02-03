1c601bd06bce2e6a97a7eb369ac5285521b27072
Anton Moskovskiy <7397158+Ant1mas@users.noreply.github.com>
2022-02-03 14:51:23 +0500

    (number-to-words-ru v1.0.5) react context changed to Redux

---
+++ number-to-words-ru/_next/static/BR8RtmmpxP8OcF8ANUMHe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/581-504b8c59be651a75.js","static/chunks/pages/index-d1e0e57b2b7111a7.js"],"/_error":["static/chunks/pages/_error-78e2be2f6fed7bca.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
