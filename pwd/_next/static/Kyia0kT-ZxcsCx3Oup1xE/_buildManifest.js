ca68a85ac308540ed0936d002d6b7c0406fda9ec
Anton Moskovskiy <7397158+Ant1mas@users.noreply.github.com>
2022-04-17 09:58:21 +0500

    (pwd v1.0.1) statistics connected

---
+++ pwd/_next/static/Kyia0kT-ZxcsCx3Oup1xE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/706-42aa600739d18bb9.js","static/chunks/pages/index-32cdf681910df4a3.js"],"/_error":["static/chunks/pages/_error-f5e38f89cdf5741c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pwd/_next/static/z3UWOretRdOdDYCWpzY_b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/706-42aa600739d18bb9.js","static/chunks/pages/index-2efaa14c471dfab4.js"],"/_error":["static/chunks/pages/_error-f5e38f89cdf5741c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
